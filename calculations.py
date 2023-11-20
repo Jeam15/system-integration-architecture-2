ee46baf56a9b84a3702894fd11aaa151c92ddc1a
Netteee <129478732+Jeam15@users.noreply.github.com>
2023-11-20 14:57:09 +0800

    Create calculations.py

---
+++ calculations.py
@@ -0,0 +1,36 @@
+# Exercise 1 "Hello World"
+
+print ("Hello World")
+
+#addition
+num1 = 20
+num2 = 30
+
+sum = num1 + num2
+
+print ("the sum is ",sum)
+
+#division
+
+num3 = 40
+num4 = 50
+
+divide = num3 / num4
+
+print ("the divide is ", divide)
+
+#subtraction
+num5 = 100
+num6 = 70
+
+subtrac = num5 - num6
+
+print ("the subtrac is ", subtrac)
+
+#multiplication
+num7 = 70
+num8 = 80
+
+multiply = num7 * num8
+
+print ("the multiply is ", multiply)
