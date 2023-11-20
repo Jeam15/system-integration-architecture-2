9355524e019e8c472e45b479b292779dcd10d714
Netteee <129478732+Jeam15@users.noreply.github.com>
2023-11-20 15:21:28 +0800

    Create 4 for loops.py

---
+++ 4 for loops.py
@@ -0,0 +1,12 @@
+#Exercise 4 for loops 
+#Create a python program tht displays all even numbers from 1 to the using for loops
+
+#Displaying even numbers from 1 to 100 using a for loop
+for i in range(1, 101):
+  if number % 2 == 0:
+    print(i)
+
+#Displaying odd numbers from 1 to 100 using a for loop
+for i in range(1, 101):
+  if number % 2 != 0:
+    print(i)
